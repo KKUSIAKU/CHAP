405ec442b353a2140b6f12486a31127a033f2d3b
kkusiaku <kusiaku.k@gmail.com>
2020-08-07 07:44:29 +0200

    setting

---
+++ charsrv.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <pthread.h>
+
+using namespace std;
+
+
+#define LISTEN_PORT    5296
+#define MAX_LINE_BUFF  1024
+
+
+
+int main() {
+
+  return 1
+}
